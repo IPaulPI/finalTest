3e16ab15424bd36c38e2f993d016b259f14b8b40
Pavel Peredrij <peredrij.pavel19@gmail.com>
2024-02-20 14:20:17 +0300

    added code to file Program.cs

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод массива с клавиатуры
+        Console.Write("Введите элементы массива через запятую: ");
+        string input = Console.ReadLine();
+        string[] originalArray = input.Split(',');
+
+        // Вызов функции для фильтрации массива
+        string[] newArray = FilterArray(originalArray);
+
+        // Вывод результата
+        Console.WriteLine("Новый массив: [" + string.Join(", ", newArray) + "]");
+    }
+
+    static string[] FilterArray(string[] inputArray)
+    {
+        // Подсчет количества элементов, удовлетворяющих условию
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива
+        string[] newArray = new string[count];
+
+        // Заполнение нового массива
+        int index = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
